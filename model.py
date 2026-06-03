daf7b177b623c9cdd4454f0369076b110874b7d8
Jona Scholz <jona.m.scholz@gmail.com>
2026-06-03 17:44:26 +0200

    rms norm

--- model.py
+++ model.py
@@ -1,20 +1,25 @@
 from jax import numpy as jnp
 from flax import nnx
 
+N_HEADS = 4
+
 
 class GPT2(nnx.Module):
+
     def __init__(self, vocab_size, D_HIDDEN, rngs):
         self.embed = nnx.Embed(num_embeddings=vocab_size, features=D_HIDDEN, rngs=rngs)
         self.attention = nnx.MultiHeadAttention(
-            num_heads=4,
+            num_heads=N_HEADS,
             in_features=D_HIDDEN,
             qkv_features=D_HIDDEN,
             decode=False,
             rngs=rngs,
         )
+        self.norm = nnx.RMSNorm(num_features=D_HIDDEN, use_scale=False, rngs=None)
 
     def __call__(self, x):
-        embedded = self.embed(x)
-        attended = self.attention(embedded, embedded, embedded)
+        x = self.embed(x)
+        x = self.attention(x, x, x)
+        x = self.norm(x)
 
-        return attended
+        return x
